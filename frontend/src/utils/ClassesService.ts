b2df98f1a65f36d686ae93d404954599839c2456
UmBarril <74427600+UmBarril@users.noreply.github.com>
2026-02-23 17:09:58 -0300

    mudando fetch api para axios

--- frontend/src/utils/ClassesService.ts
+++ frontend/src/utils/ClassesService.ts
@@ -1,41 +1,26 @@
+import axios from "axios";
 import type { TurmaResponse } from "../types/StudentClassesResponse";
 
 const API_URL = import.meta.env.VITE_API_URL || "http://localhost:8085";
 
 export async function fetchClasses(matricula: string, access_token: string): Promise<TurmaResponse[]> {
     const body = {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${access_token}`,
-            "Content-Type": "application/json",
-            "User-Agent": "Dart/2.10 (dart:io)",
-            "Accept-Encoding": "gzip",
-        },
+        headers: { 
+            Authorization: `Bearer ${access_token}` 
+        }
     }
 
-    const response = await fetch(`${API_URL}/sigaa/mobile/discente/${matricula}/turmas`, body);
-    if (!response.ok) {
-        throw new Error(`Failed to fetch classes: ${response.statusText}`);
-    }
-
-    return response.json();
+    const response = await axios.get(`${API_URL}/sigaa/mobile/discente/${matricula}/turmas`, body);
+    return response.data;
 }
 
 export async function fetchClassDetails(matricula: string, idTurma: string, access_token: string): Promise<string> {
     const body = {
-        method: "GET",
-        headers: {
-            Authorization: `Bearer ${access_token}`,
-            "Content-Type": "application/json",
-            "User-Agent": "Dart/2.10 (dart:io)",
-            "Accept-Encoding": "gzip",
-        },
-    }
-    const response = await fetch(`${API_URL}/sigaa/mobile/discente/${matricula}/turma/${idTurma}`, body);
-    
-    if (!response.ok) {
-        throw new Error(`Failed to fetch class details: ${response.statusText}`);
+        headers: { 
+            Authorization: `Bearer ${access_token}` 
+        }
     }
 
-    return await response.json();
+    const response = await axios.get(`${API_URL}/sigaa/mobile/discente/${matricula}/turma/${idTurma}`, body);
+    return response.data;
 }
--- frontend/src/utils/LoginService.ts
+++ frontend/src/utils/LoginService.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import type { AuthResponse } from "../types/AuthResponse";
 
 export const fetchAuthData = async (username: string, password: string): Promise<AuthResponse> => {
@@ -9,22 +10,13 @@ export const fetchAuthData = async (username: string, password: string): Promise
     });
 
     const authUrl = import.meta.env.VITE_API_URL || "http://localhost:8085";
-    const response = await fetch(`${authUrl}/auth-server/oauth/token`, {
-        method: "POST",
+    const response = await axios.post(`${authUrl}/auth-server/oauth/token`, bodyParams, {
         headers: {
-            Authorization:
-            "Basic c2lnYWEtZGlzY2VudGUtbW9iaWxlOjZkMDYyODBkMTc5MzY3ZjhmM2I3ZjhmYmJjNmJmOTgx",
             "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
             Accept: "application/json",
         },
-        body: bodyParams.toString(),
     });
 
-    if (!response.ok) {
-        const text = await response.text();
-        throw new Error(`Auth failed with status: ${response.status}. ${text}`);
-    }
-
-    const data: AuthResponse = await response.json();
+    const data: AuthResponse = response.data;
     return data;
 }
