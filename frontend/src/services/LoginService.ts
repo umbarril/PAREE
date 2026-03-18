31a8a8528557e6f50baadcbccdeb50f0970d70cc
UmBarril <74427600+UmBarril@users.noreply.github.com>
2026-03-18 19:13:33 -0300

    withcredentails

--- frontend/src/services/LoginService.ts
+++ frontend/src/services/LoginService.ts
@@ -13,7 +13,7 @@ export const fetchAuthData = async (username: string, password: string): Promise
         headers: {
             "Content-Type": "application/x-www-form-urlencoded; charset=utf-8",
             Accept: "application/json",
-        },
+        }, withCredentials: true,
     });
 }
 
@@ -21,4 +21,4 @@ export const fetchAuthData = async (username: string, password: string): Promise
 export const fetchLogout = async (): Promise<AxiosResponse<void>> => api.post(`/auth/logout`, {}, {});
 
 // todo: por enquanto ele ignora o método http, mas no futuro pode ser necessário usar um método específico para fetchMe
-export const fetchMe = async (): Promise<AxiosResponse<AuthResponse>> => api.get(`/auth/me`, {});
+export const fetchMe = async (): Promise<AxiosResponse<AuthResponse>> => api.get(`/auth/me`, {withCredentials: true});
